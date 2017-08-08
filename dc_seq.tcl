264bf9a605f7f4ed10404aee0c9eed1a70086761
Charles <charlesjiangxm@gmail.com>
2017-08-08 22:25:58 +0800

    Update dc_seq.tcl

--- dc_seq.tcl
+++ dc_seq.tcl
@@ -15,10 +15,10 @@ sh mkdir WORK
 define_design_lib WORK -path ./WORK
 
 # scripts of step 10, enter all the files excpet testbench #change here
-analyze -library WORK -format sverilog {../rtl/shiftandadd.v}
+analyze -library WORK -format sverilog {../rtl/PriorityArbiter.v ../rtl/RoundRobinArbiter.v }
 
 # scripts of step 11, the name should be the top instance name #change here
-elaborate shiftandadd -architecture verilog -library DEFAULT -update
+elaborate RoundRobinArbiter -architecture verilog -library DEFAULT -update
 
 # scripts of step 12
 link
@@ -28,53 +28,53 @@ uplevel #0 check_design
 
 # scripts of step 14, name can be arbitrary
 # You can replce "cla16" globally here, and no need for change afterwards #change here
-write -hierarchy -format ddc -output ../ddc/shiftandadd.ddc
+write -hierarchy -format ddc -output ../ddc/RoundRobinArbiter.ddc
 
 # scripts of step 15: the input rising and falling transition delay is 0.01ns
 change_selection [all_inputs]
-set_drive [expr 0.01] [all_inputs]
+set_drive [expr 0.0] [all_inputs]
 
 # scripts of step 16: the output loading is 20fF
 change_selection [all_outputs]
-set_load [expr 0.02] [all_outputs]
+set_load [expr 0.0] [all_outputs]
 
 # scripts of step 17, if there is a clock signal in your design, please append "{clk(or the name of the clock you use)}" at the end, time unit: ns
 # Combination Logif Use this:
 # create_clock -name "vclk" -period 2 -waveform { 1 2  }
-# Sequential Logic with the clk port name as clk_fs, frequency 0.768ns use this (don't touch network is to let synthesize tool don't modify the clk signal)
-create_clock -name "clk_fs" -period 0.768 -waveform { 0.384 0.768  }  { clk_fs  }
-set_dont_touch_network  [ find clock clk_fs ]
+# Sequential Logic with the clk port name as clk, frequency 0.768ns use this (don't touch network is to let synthesize tool don't modify the clk signal)
+create_clock -name "clk" -period 2 -waveform { 1 2  }  [get_ports clk]
+#set_dont_touch_network  [ find clock clk ]
 
 
 # scripts of step 18: set input rising and falling delay range, here is 0ns
 change_selection [all_inputs]
-set_input_delay -clock clk_fs -add_delay  -max -rise 0 [all_inputs]
-set_input_delay -clock clk_fs -add_delay -max -fall 0 [all_inputs]
-set_input_delay -clock clk_fs -add_delay -min -rise 0 [all_inputs]
-set_input_delay -clock clk_fs -add_delay  -min -fall 0 [all_inputs]
+set_input_delay -clock clk -add_delay  -max -rise 0 [all_inputs]
+set_input_delay -clock clk -add_delay -max -fall 0 [all_inputs]
+set_input_delay -clock clk -add_delay -min -rise 0 [all_inputs]
+set_input_delay -clock clk -add_delay  -min -fall 0 [all_inputs]
 
 # scripts of step 19: set output rising and falling delay range, here is 0.05ns for all
 change_selection [all_outputs]
-set_output_delay -clock clk_fs -add_delay  -max -rise 0.05 [all_outputs]
-set_output_delay -clock clk_fs -add_delay -max -fall 0.05 [all_outputs]
-set_output_delay -clock clk_fs -add_delay -min -rise 0.05 [all_outputs]
-set_output_delay -clock clk_fs -add_delay  -min -fall 0.05 [all_outputs]
+set_output_delay -clock clk -add_delay  -max -rise 0.0 [all_outputs]
+set_output_delay -clock clk -add_delay -max -fall 0.0 [all_outputs]
+set_output_delay -clock clk -add_delay -min -rise 0.0 [all_outputs]
+set_output_delay -clock clk -add_delay  -min -fall 0.0 [all_outputs]
 
 # scripts of step 20: specify wire load constraint
 set_wire_load_model -name 1K_hvratio_1_2 -library NangateOpenCellLibrary
 
 # scripts of step 21: This is to specify the design should be speed oriented, if you want it to be area oriented, type: "set_max_area 0"
-set_max_delay 0 -from [all_inputs] -to [all_outputs]
+#set_max_delay 0 -from [all_inputs] -to [all_outputs]
 
 # scripts of step 22: save the constrained design, changes the name here #change here
-write -hierarchy -format ddc -output ../ddc/shiftandadd_constriant.ddc 
+write -hierarchy -format ddc -output ../ddc/RoundRobinArbiter_constriant.ddc 
 
 # scripts of step 23: Synthesize
-compile -extract_map
-# compile_ultra -exact_map
+# compile -exact_map
+compile_ultra
 
 # scripts of step 24: save the synthesized design, changes the name here #change here
-write -hierarchy -format ddc -output ../ddc/shiftandadd_synthesized.ddc
+write -hierarchy -format ddc -output ../ddc/RoundRobinArbiter_synthesized.ddc
 
 # scripts of step 26
 report_timing >> ../rpt/timing.rpt
@@ -83,17 +83,17 @@ report_power >> ../rpt/power.rpt
 report_clock >> ../rpt/clock.rpt
 
 # scripts of step 27 #change here
-change_names -rule vhdl -hierarchy
-write -format vhdl -hierarchy -output ../net/shiftandadd.vhd
+#change_names -rule vhdl -hierarchy
+#write -format vhdl -hierarchy -output ../net/RoundRobinArbiter.vhd
 
 # scripts of step 28 #change here
 change_names -rules verilog -hierarchy
-write -format verilog -hierarchy -output ../net/shiftandadd.v
+write -format verilog -hierarchy -output ../net/RoundRobinArbiter.v
 
 # scripts of step 29 #change here
-write_sdf ../net/shiftandadd.sdf
+write_sdf ../net/RoundRobinArbiter.sdf
 
 # scripts of step 30 #change here
-write_sdc ../net/shiftandadd.sdc
+write_sdc ../net/RoundRobinArbiter.sdc
 
 quit
