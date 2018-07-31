d97d3474c61fc694e5e3e8282f73543b109a2f6e
Charles <charlesjiangxm@gmail.com>
2018-07-31 11:00:41 +0800

    Update testbench.v

--- testbench.v
+++ testbench.v
@@ -1,69 +1,52 @@
-// ---------------------------------------------------
-// Description: General Purpose testbench
-// ---------------------------------------------------
-`timescale 1 ns / 1 ps
-module testbench;
-	
-// Anotate sdf file
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: HKUST
+// Engineer: Charles JIANG
+// 
+// Create Date: 
+// Design Name: 
+// Module Name: 
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+`include "define.vh"
+
+module param_calc_testbench;
+
+localparam CLK_PERIOD   = 10.0;
+localparam RST_TIME     = CLK_PERIOD * 3
+
+// signal lists ------------------------------------------------------------------
+reg clk, rst_n;
+
+// instantiation -----------------------------------------------------------------
+
+// clock and reset ---------------------------------------------------------------
 initial begin
-$sdf_annotate("../net/RoundRobinArbiter.sdf", uut);
+  clk = 0;
+  forever #(CLK_PERIOD/2.0) clk = ~clk;
 end
 
-// Modify your clk period here, unit: ns
-localparam CLK_PERIOD = 1;
-
-// Modify your parameters here
-parameter			N = 4;
-
-// Modify your ports here
-reg				clk;          // system clock
-reg				rst_n;          // system reset
-reg	[N-1:0]			request;      // request
-wire	[N-1:0]			grant;         // grant
-	
-RoundRobinArbiter #(
-	.N		(4)
-) uut
-(
-	.clk			(clk),           
-	.rst_n			(rst_n),            
-	.request		(request),
-	.grant			(grant)
-);
-
-// CLk initial block
-initial begin
-	clk = 1'b0;
-	forever #(CLK_PERIOD/2.0) begin
-	clk = ~clk;
-	end
+initial begin 
+    rst_n = 0;
+    #RST_TIME rst_n = 1;
 end
 
-// rst_n initial block
-initial begin
-	rst_n = 1'b1;
-	#(CLK_PERIOD*3);
-	rst_n = 1'b0;
-end
+// processing ---------------------------------------------------------------------
+initial begin 
+    #RST_TIME
+    
+    
+end 
 
-// data initial block
-initial begin
-	request = 3'd0;
-	# 100
-	request = 3'd1;
-	# 100
-	request = 3'd2;
-	# 100
-	request = 3'd3;
-	# 100
-	request = 3'd4;	
-	# 100
-	request = 3'd5;
-	# 100
-	request = 3'd6;
-	# 100
-	request = 3'd7;
-	$stop;
-end
 
 endmodule
