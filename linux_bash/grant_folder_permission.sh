4cbb3f9474b13ee3f1b5628daef586c55cb967ba
Charles <charlesjiangxm@gmail.com>
2018-11-01 12:17:05 +0800

    aSD

--- linux_bash/grant_folder_permission.sh
+++ linux_bash/grant_folder_permission.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# grant all users the read write permission to the directory and sub-directories
+sudo chmod -R 0777 /directory
+
 # grant read write permission to a user without changing the ownership
 setfacl -m u:username:rwx myfolder
 
 # grant the ownership of a folder
-sudo chown -R username: directory
+sudo chown -R username: /directory
